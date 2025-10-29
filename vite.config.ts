74b211c36d4c5481db9010850a0fa5e9e64eb278
Abel <74651681+AbelJSeba@users.noreply.github.com>
2025-10-29 09:38:02 -0400

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 export default defineConfig({
   plugins: [react()],
+  base: "./", // <-- critical for Cloudflare Pages
+  build: {
+    outDir: "dist", // <-- make sure this matches your Cloudflare output directory
+  },
   resolve: {
     alias: {
       "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
     },
   },
-}) 
+});
